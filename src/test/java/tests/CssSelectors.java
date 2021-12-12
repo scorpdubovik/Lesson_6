33761c759d0a3848743d25677c7d33b584b67aff
Andrey <scorpdubovik@gmail,com>
2021-12-12 14:23:56 +0300

    Учеба по cssSelectors

--- src/test/java/tests/CssSelectors.java
+++ src/test/java/tests/CssSelectors.java
@@ -17,7 +17,7 @@ public class CssSelectors {
         By tagAttributeSelector = By.cssSelector("label[for='name']"); //Поиск по tag и аттрибуту с значением
         By tagAttributeSelector1 = By.cssSelector("label[for]"); //Поиск по tag и присутсвием аттрибута
 
-        By multipleClassesSelector = By.cssSelector(".column.overflow-content']"); //Поиск по двум классам в элементе
+        By multipleClassesSelector = By.cssSelector(".column.overflow-content"); //Поиск по двум классам в элементе
         By simpleHierarchicalSelector = By.cssSelector(".table .content-inner"); //Поиск простого дочернего элемента
 
         By fullHierarchicalSelector = By.cssSelector("body .table .content-inner"); //Использование 3х уровневой иерархии для поиска дочернего элемента
@@ -25,9 +25,9 @@ public class CssSelectors {
         By searchLikeContainsSelector = By.cssSelector("form[action*='admin']"); //Поиск с вхождением подстроки в значении аттрибута
         By searchByWordSelector = By.cssSelector("div[class~='column']"); //Поиск с вхождением слова в значении аттрибута
 
-        By valueStartsFromSelector = By.cssSelector("a[href^='https://']"); //Поиск с элемента с аттрибутом значение которого начинается с
+        By valueStartsFromSelector = By.cssSelector("a[href^='https://']"); //Поиск эл  емента с аттрибутом значение которого начинается с
 
-        By valueEndsWithSelector = By.cssSelector("a[href$='@gmail.com']"); //Поиск с элемента с аттрибутом значение которого аканчивается на
+        By valueEndsWithSelector = By.cssSelector("a[href$='@gmail.com']"); //Поиск элемента с аттрибутом значение которого аканчивается на
 
         By childRightAfterParentSelector = By.cssSelector("ul>li"); //Поиск дочернего li у которого родитель ul
 
