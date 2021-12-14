288ddb2af62d9cb5292ec6fee9c283f587bb2351
Andrey <scorpdubovik@gmail,com>
2021-12-14 23:41:48 +0300

    Домашка по xPathSelectors в работе

--- src/test/java/tests/HomeWorkXpathSelectors.java
+++ src/test/java/tests/HomeWorkXpathSelectors.java
@@ -2,4 +2,52 @@ package tests;
 
 public class HomeWorkXpathSelectors {
 
+    static String URL = "http://www.ktto.com.ua/calculation/p_kompensator";
+
+    private static String absolutePath = "//html/head/script"; // Поиск элемента по абсолютному пути
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    //Использование following - Выбирает всё в документе после закрытия тэга текущего узла
+    private static String path4_1 = "//div[@id='large_img']/following::strong";
+    private static String path4_2 = "//div[@id='large_img']/following::div/*[@style=\"margin-left:5px;\"])[2]";
+
+
+
 }
